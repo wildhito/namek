4a826d28467ce91ce5ce05dddcbf691cc1197f72
wildhito <mic-chan@hotmail.fr>
2016-12-26 16:28:40 +0100

    #2 Begin to configure security

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * Security controller.
+ *
+ */
+class SecurityController extends Controller
+{
+    /**
+     * Login action
+     *
+     * @Route("/login/", name="login")
+     */
+    public function loginAction(Request $request)
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+        return $this->render('security/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ));
+    }
+}
+
